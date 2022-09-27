6d17fecf6c541f13570949cf2e6c94f41285f977
Nig3lTseng <nigeltseng8@gmail.com>
2022-09-27 23:13:28 +0800

    Initial Commit

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="min-h-[10vh] w-full p-8 flex items-center justify-center">
+      <p className="text-sm">
+        Made with 💖 from{" "}
+        <a
+          href="https://www.linkedin.com/in/nigel-tseng-9797a6245/"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          Alix & Nigel Tseng
+        </a>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Hero.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import invoice from "../images/invoice.svg";
+
+const Hero = () => {
+  return (
+    <div className="md:min-h-[90vh] w-full md:px-8 px-2 py-[80px] flex lg:flex-row items-center justify-center flex-col">
+      <div
+        className="pl-3 my-4 lg:mb-1 lg:text-left pr-6 mb-10"
+        style={{ flex: 0.6 }}
+      >
+        <h2 className="text-3xl font-bold mb-4 md:text-4xl">
+          Client & Talent manager for Outsource.io
+        </h2>
+        <p className="opacity-70 mb-4 text-sm md:text-base leading-loose">
+          Specially for Alix & Nigel, clients and talents managed here with
+          React-based frontend backend authentication, data collection and
+          management with Google Firebase. Login currently only allowed for
+          administrators currently!
+        </p>
+
+        <Link to="/login">
+          <button className="rounded w-[200px] px-2 py-3 bg-blue-500 text-gray-50">
+            LOG IN
+          </button>
+        </Link>
+      </div>
+
+      <div className="flex items-center justify-center" style={{ flex: 0.4 }}>
+        <img src={invoice} alt="Print invoices" className="w-2/3 lg:w-full" />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
