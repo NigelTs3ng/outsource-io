c0cd4121467ce776285ee964ee97a701f08f121f
Nig3lTseng <nigeltseng8@gmail.com>
2022-09-30 17:53:02 +0800

    History View Page

---
+++ src/pages/HistoryView.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Footer from "../components/Footer";
+import Nav from "../components/Nav";
+
+const HistoryView = () => {
+  return (
+    <div>
+      <Nav />
+      <h2>History Page</h2>
+      <Footer />
+    </div>
+  );
+};
+
+export default HistoryView;
